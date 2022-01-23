024da39b7bdc30bdd520a04070ebf7c540dd2059
Valentin <64141679+OnyxKun@users.noreply.github.com>
2022-01-23 14:48:29 +0100

    Update boot.py

--- Adafruit_Feather_M4_Express_(Ruche)/boot.py
+++ Adafruit_Feather_M4_Express_(Ruche)/boot.py
@@ -9,3 +9,5 @@ switch.pull = digitalio.Pull.UP
 # If the D0 is connected to ground with a wire
 # CircuitPython can write to the drive
 storage.remount("/", switch.value)
+
+#cela permet d'écrire les données des capteurs sur un fichier texte pour pouvoir les récupérer avec le raspberry.
